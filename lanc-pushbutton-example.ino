3a0e536dc1b20ff96e42dcea19af5a17bc8fb08f
Jason Osmann <136187135+DietToms@users.noreply.github.com>
2026-06-12 23:23:02 -0700

    Add files via upload

---
+++ lanc-pushbutton-example.ino
@@ -0,0 +1,52 @@
+/*
+ * LancRemote Example: Pushbutton Toggle
+ * Demonstrates basic hardware control of a LANC recorder.
+ * * Hardware Setup:
+ * - D9: Connect to the Tip of the 2.5mm LANC jack.
+ * - D2: Connect to a momentary pushbutton. Connect the other side to GND.
+ */
+
+#include "LancMaster.h"
+
+#define LANC_TX_PIN 9
+#define BUTTON_PIN  2
+#define LED_PIN     13
+
+LancRemote lanc(LANC_TX_PIN);
+
+bool isRecording = false;
+bool lastButtonState = HIGH; // Tracks previous loop state for edge detection
+
+void setup() {
+    pinMode(BUTTON_PIN, INPUT_PULLUP);
+    pinMode(LED_PIN, OUTPUT);
+    digitalWrite(LED_PIN, LOW);
+
+    // Boot the bus and send the wake-up sequence
+    lanc.begin();
+}
+
+void loop() {
+    // 1. Maintain the 60Hz PHY heartbeat 
+    lanc.loop();
+
+    // 2. Read the physical button
+    bool currentButtonState = digitalRead(BUTTON_PIN);
+
+    // Detect falling edge (button pressed to GND)
+    // NOTE: Add software debouncing in production to prevent contact-bounce triggers
+    if (lastButtonState == HIGH && currentButtonState == LOW) {
+        
+        isRecording = !isRecording; // Toggle state
+
+        if (isRecording) {
+            lanc.setCommand(CMD_RECORD);
+            digitalWrite(LED_PIN, HIGH);
+        } else {
+            lanc.setCommand(CMD_STANDBY);
+            digitalWrite(LED_PIN, LOW);
+        }
+    }
+
+    lastButtonState = currentButtonState;
+}
